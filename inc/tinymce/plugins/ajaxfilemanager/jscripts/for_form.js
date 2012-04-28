de5d32c06b06e42384c2ca6946c55890542f798e
Koma <andre_zeisberg@yahoo.de>
2012-04-28 16:58:38 +0200

    Revert "fix download path problem"
    This reverts commit b80800c23cc92d8c484791ad35baaba2ee381fd3.
    
    Conflicts:
    
    	inc/tinymce/plugins/ajaxfilemanager/jscripts/for_form.js

--- inc/tinymce/plugins/ajaxfilemanager/jscripts/for_form.js
+++ inc/tinymce/plugins/ajaxfilemanager/jscripts/for_form.js
@@ -1,22 +1,7 @@
 function selectFile(url) {
     var fileName = '';
     if (url.length == 0) {
-function selectFile(url) {
-    var fileName = '';
-    if (url.length == 0) {
-        fileName = '';
-    } else {
-        var pos = url.lastIndexOf("/inc/tinymce_files");
-        if (pos >= 0) {
-            fileName = ".." + url.substr(pos);
-        } else {
-            fileName = url;
-        }
-    }
-    window.opener.document.getElementById(elementId).value = fileName;
-    window.close();
-}
-
+        fileName =  ''; 
     } else {
         var pos = url.lastIndexOf("/inc/tinymce_files");
         if (pos >= 0) {
