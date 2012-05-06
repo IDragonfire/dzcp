a0b987ff5fa37ece33502248610dbf7ff754e1af
IDragonfire <bastian616-git@yahoo.de>
2012-05-06 21:10:13 +0200

    change version file

--- inc/_version.php
+++ inc/_version.php
@@ -1,6 +1,6 @@
 <?php 
-  define('_version', '1.5.5.2');
-  define('_release', '12.03.2011');
+  define('_version', '1.6');
+  define('_release', '??.??.2012');
   
   $cp_color = '#FFFFFF';  ###########################################################################
                            # Hex Farbcode der Hintergrundfarbe Copyrightlinks am Ende der Homepage #
