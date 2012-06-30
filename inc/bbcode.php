34f96a2e09e2d4b306b496cff94e0a8a57266567
IDragonfire <bastian616-git@yahoo.de>
2012-06-30 15:49:56 +0200

    rework #82 url autolink, use wordpress make_clickable function

--- inc/bbcode.php
+++ inc/bbcode.php
@@ -468,19 +468,63 @@ function re_bbcode($txt)
 
 	return $txt;
 }
-//URLs automatisch verlinken
-function autolink($text) {
-  	$pattern = '#(^|[^\"=]{1})(http://|https://|ftp://|www.)([^\s<>]+)([\s\n<>]|$)#sm';
- 	$text = preg_replace($pattern,"\\1<a target=\"_blank\" href=\"\\2\\3\">\\2\\3</a>\\4",$text);
-	$text = str_replace("href=\"www.", "href=\"http://www.",$text);
-	return $text;
+/* START # from wordpress under GBU GPL license
+   URL autolink function */
+function _make_url_clickable_cb($matches) {
+	$ret = '';
+	$url = $matches[2];
+ 
+	if ( empty($url) )
+		return $matches[0];
+	// removed trailing [.,;:] from URL
+	if ( in_array(substr($url, -1), array('.', ',', ';', ':')) === true ) {
+		$ret = substr($url, -1);
+		$url = substr($url, 0, strlen($url)-1);
+	}
+	return $matches[1] . "<a href=\"$url\" rel=\"nofollow\">$url</a>" . $ret;
 }
+ 
+function _make_web_ftp_clickable_cb($matches) {
+	$ret = '';
+	$dest = $matches[2];
+	$dest = 'http://' . $dest;
+ 
+	if ( empty($dest) )
+		return $matches[0];
+	// removed trailing [,;:] from URL
+	if ( in_array(substr($dest, -1), array('.', ',', ';', ':')) === true ) {
+		$ret = substr($dest, -1);
+		$dest = substr($dest, 0, strlen($dest)-1);
+	}
+	return $matches[1] . "<a href=\"$dest\" rel=\"nofollow\">$dest</a>" . $ret;
+}
+ 
+function _make_email_clickable_cb($matches) {
+	$email = $matches[2] . '@' . $matches[3];
+	return $matches[1] . "<a href=\"mailto:$email\">$email</a>";
+}
+ 
+function make_clickable($ret) {
+	$ret = ' ' . $ret;
+	// in testing, using arrays here was found to be faster
+	$ret = preg_replace_callback('#([\s>])([\w]+?://[\w\\x80-\\xff\#$%&~/.\-;:=,?@\[\]+]*)#is', '_make_url_clickable_cb', $ret);
+	$ret = preg_replace_callback('#([\s>])((www|ftp)\.[\w\\x80-\\xff\#$%&~/.\-;:=,?@\[\]+]*)#is', '_make_web_ftp_clickable_cb', $ret);
+	$ret = preg_replace_callback('#([\s>])([.0-9a-z_+-]+)@(([0-9a-z-]+\.)+[0-9a-z]{2,})#i', '_make_email_clickable_cb', $ret);
+ 
+	// this one is not in an array because we need it to run last, for cleanup of accidental links within links
+	$ret = preg_replace("#(<a( [^>]+?>|>))<a [^>]+?>([^>]+?)</a></a>#i", "$1$3</a>", $ret);
+	$ret = trim($ret);
+	return $ret;
+}
+
+/* END # from wordpress under GBU GPL license */
+
 //Diverse BB-Codefunktionen
 function bbcode($txt, $tinymce=0, $no_vid=0,$ts=0,$nolink=0)
 {
   global $settings;
   if($no_vid == 0 && $settings['urls_linked'] == 1 && $nolink == 0) {
-	  $txt = autolink($txt);
+	  $txt = make_clickable($txt);
   }
   $txt = str_replace("\\n","<br />",$txt);
   $txt = BadwordFilter($txt);
