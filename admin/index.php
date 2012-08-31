820d5fd55bbc8f0d58e114c99b0f96fcfd3625cd
Lucas <godkiller_nt@hammermaps.de>
2012-08-31 22:57:17 +0200

    Fix Notice #2
    Notice: Undefined index: admin in admin\index.php on line 23
    Notice: Undefined variable: settingsmenu in admin\index.php on line 60
    Notice: Undefined variable: contentmenu in admin\index.php on line 60
    Notice: Undefined variable: rootmenu in admin\index.php on line 60
    Notice: Undefined variable: wysiwyg in admin\index.php on line 116
    Notice: Undefined variable: fkats in admin\index.php on line 94
    Notice: Undefined variable: nkats in admin\index.php on line 95
    Notice: Undefined variable: radmin1 in admin\index.php on line 106
    Notice: Undefined variable: radmin2 in admin\index.php on line 107
    Notice: Undefined variable: adminc1 in admin\index.php on line 108
    Notice: Undefined variable: adminc2 in admin\index.php on line 109
    Notice: Undefined variable: show in admin\index.php on line 110

--- admin/index.php
+++ admin/index.php
@@ -1,119 +1,125 @@
 <?php
 ## OUTPUT BUFFER START ##
 include("../inc/buffer.php");
+
 ## INCLUDES ##
 include(basePath."/inc/config.php");
 include(basePath."/inc/bbcode.php");
+
 ## SETTINGS ##
 $time_start = generatetime();
 lang($language);
 $where = _site_config;
 $dir = "admin";
+$show = "";
+
 ## SECTIONS ##
-  $check = db("SELECT s1.user FROM ".$db['permissions']." s1, ".$db['users']." s2
-               WHERE s1.user = '".$userid."'
-               AND s2.id = '".intval($userid)."'
-               AND s2.pwd = '".$_SESSION['pwd']."'");
+$check = db("SELECT s1.user FROM ".$db['permissions']." s1, ".$db['users']." s2
+             WHERE s1.user = '".$userid."'
+             AND s2.id = '".intval($userid)."'
+             AND s2.pwd = '".$_SESSION['pwd']."'");
 
-  if(!admin_perms($_SESSION['id']))
-  {
+if(!admin_perms($_SESSION['id']))
     $index = error(_error_wrong_permissions, 1);
-  } else {
+else 
+{
     define('_adminMenu', true);
-    if(file_exists(basePath.'/admin/menu/'.strtolower($_GET['admin']).'.php'))
+    $settingsmenu = ""; $contentmenu = ""; $rootmenu = ""; $wysiwyg = false;
+    $radmin1 = ''; $radmin2 = ''; $adminc1 = ''; $adminc2 = '';
+    
+    if(isset($_GET['admin']))
     {
-      include(basePath.'/admin/menu/'.strtolower($_GET['admin']).'.php');
+        if(file_exists(basePath.'/admin/menu/'.strtolower($_GET['admin']).'.php'))
+            include(basePath.'/admin/menu/'.strtolower($_GET['admin']).'.php');
     }
-
-//Site Permissions
-    $qry = db("SELECT * FROM ".$db['permissions']."
-               WHERE user = '".intval($userid)."'");
-    $check = _fetch($qry);
-
-    unset($amenu);
+    
+    //Site Permissions
+    $check = _fetch(db("SELECT * FROM ".$db['permissions']." WHERE user = '".intval($userid)."'"));
+    
+    $amenu = array();
     $files = get_files(basePath.'/admin/menu/',false,true,array('php'));
     foreach($files AS $file)
     {
         $nav = file(basePath.'/admin/menu/'.$file);
         $navType = trim(str_replace('// Typ:', '', $nav[2]));
         $navPerm = trim(str_replace('// Rechte:', '', $nav[3]));
-
+        
         $file = str_replace('.php', '', $file);
         @eval("\$link = _config_".$file.";");
         @eval("\$permission = ".$navPerm.";");
-        
-		foreach($picformat AS $end)
-		{
-			if(file_exists(basePath.'/admin/menu/'.$file.'.'.$end))
-				break;
-		}
-
-        if(!empty($navType) && !empty($navPerm) && $permission)
+            
+        foreach($picformat AS $end)
         {
-        	$amenu[$navType][$link] = show("['[link]','?admin=[name]','background-image:url(menu/[name].".$end.");'],\n", array("link" => $link, 'name' => $file));
+            if(file_exists(basePath.'/admin/menu/'.$file.'.'.$end))
+                break;
         }
+    
+        if(!empty($navType) && !empty($navPerm) && $permission)
+            $amenu[$navType][$link] = show("['[link]','?admin=[name]','background-image:url(menu/[name].".$end.");'],\n", array("link" => $link, 'name' => $file));
     }
-
+    
     foreach($amenu AS $m => $k)
     {
-      natcasesort($k);
-      foreach($k AS $l) $$m .= $l;
+        natcasesort($k);
+        foreach($k AS $l) $$m .= $l;
     }
-
+    
     if(empty($rootmenu))
     {
-      $radmin1 = '/*'; $radmin2 = '*/';
+        $radmin1 = '/*'; $radmin2 = '*/';
     }
-
+    
     if(empty($settingsmenu))
     {
-      $adminc1 = '/*'; $adminc2 = '*/';
+        $adminc1 = '/*'; $adminc2 = '*/';
     }
-      
+    	
     if(function_exists('fsockopen'))
     {
-      $dzcp_v = fileExists("http://www.dzcp.de/version.txt");
-      if($dzcp_v <= _version)
-      {
-        $version = '<b>'._akt_version.': <span class="fontGreen">'._version.'</span></b>';
-        $old = "";
-      } else {
-        $version = "<a href=\"http://www.dzcp.de\" target=\"_blank\" title=\"external Link: www.dzcp.de\"><b>"._akt_version.":</b> <span class=\"fontRed\">"._version."</span></a>";
-        $old = "_old";
-      }
-	  
-	  $dzcp_news = @file_get_contents('http://www.dzcp.de/dzcp_news.php');
+        $dzcp_v = fileExists("http://www.dzcp.de/version.txt");
+        if($dzcp_v <= _version)
+        {
+            $version = '<b>'._akt_version.': <span class="fontGreen">'._version.'</span></b>';
+            $old = "";
+        } 
+        else 
+        {
+            $version = "<a href=\"http://www.dzcp.de\" target=\"_blank\" title=\"external Link: www.dzcp.de\"><b>"._akt_version.":</b> <span class=\"fontRed\">"._version."</span></a>";
+            $old = "_old";
+        }
+    	  
+        $dzcp_news = @file_get_contents('http://www.dzcp.de/dzcp_news.php');
     }
-    if(@file_exists(basePath."/_installer") && $chkMe == 4)
-		{
-			$index = _installdir;
-		} else {
-    
-    $index = show($dir."/admin", array("head" => _config_head,
-                                       "forumkats" => $fkats,
-                                       "newskats" => $nkats,
-                                       "version" => $version,
-                                       "old" => $old,
-                                       "dbase" => _stats_mysql,
-                                       "einst" => _config_einst,
-                                       "content" => _content,
-                                       "newsticker" => '<div style="padding:3px">'.(empty($dzcp_news) ? '' : '<b>DZCP News:</b><br />').'<div id="dzcpticker">'.$dzcp_news.'</div></div>',
-                                       "rootadmin" => _rootadmin,
-                                       "rootmenu" => $rootmenu,
-                                       "settingsmenu" => $settingsmenu,
-                                       "contentmenu" => $contentmenu,
-                                       "radmin1" => $radmin1,
-                                       "radmin2" => $radmin2,
-                                       "adminc1" => $adminc1,
-                                       "adminc2" => $adminc2,
-																			 "show" => $show));
-		}
-  }
+        
+    if(@file_exists(basePath."/_installer1") && $chkMe == 4)
+        $index = _installdir;
+    else 
+    {
+        $index = show($dir."/admin", array( "head" => _config_head,
+                                            "version" => $version,
+                                            "old" => $old,
+                                            "dbase" => _stats_mysql,
+                                            "einst" => _config_einst,
+                                            "content" => _content,
+                                            "newsticker" => '<div style="padding:3px">'.(empty($dzcp_news) ? '' : '<b>DZCP News:</b><br />').'<div id="dzcpticker">'.$dzcp_news.'</div></div>',
+                                            "rootadmin" => _rootadmin,
+                                            "rootmenu" => $rootmenu,
+                                            "settingsmenu" => $settingsmenu,
+                                            "contentmenu" => $contentmenu,
+                                            "radmin1" => $radmin1,
+                                            "radmin2" => $radmin2,
+                                            "adminc1" => $adminc1,
+                                            "adminc2" => $adminc2,
+                                            "show" => $show));
+    }
+}
+
 ## SETTINGS ##
 $time_end = generatetime();
 $time = round($time_end - $time_start,4);
 $title = $pagetitle." - ".$where."";
 page($index, $title, $where ,$time,$wysiwyg);
+
 ## OUTPUT BUFFER END ##
 gz_output();
 ?>
