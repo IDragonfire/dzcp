77af080fdcd3f116cd04df7d656b5d6f9d1643ca
Lucas <godkiller_nt@hammermaps.de>
2012-09-27 20:44:12 +0200

    Remove old lang_ code from function page()

--- inc/bbcode.php
+++ inc/bbcode.php
@@ -2068,8 +2068,6 @@ function page($index,$title,$where,$time,$wysiwyg='',$index_templ=false)
 		{
 			if(strstr($pholder[$i], 'nav_')) 
 				$arr[$pholder[$i]] = navi($pholder[$i]);
-			else if(strstr($pholder[$i], 'lang_')) 
-			    $arr[$pholder[$i]] = fetchLanguage(substr($pholder[$i], 3));
 			else
 			{
 				if(@file_exists(basePath.'/inc/menu-functions/'.$pholder[$i].'.php')) 
