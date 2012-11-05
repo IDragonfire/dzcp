30f9627711af814532ebd3c629d8ec4310fa354a
Lucas <godkiller_nt@hammermaps.de>
2012-11-05 12:40:46 +0100

    Change include to require_once

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 ob_start();
   define('basePath', dirname(__FILE__));
-  
   if(file_exists(basePath."/inc/mysql.php"))
       require_once(basePath."/inc/mysql.php");
   else
@@ -11,10 +10,9 @@ ob_start();
         header('Location: _installer/index.php');
 	else 
 	{
-		include(basePath."/inc/config.php");
-		include(basePath."/inc/bbcode.php");
+		require_once(basePath."/inc/config.php");
+		require_once(basePath."/inc/bbcode.php");
 		header('Location: '.(empty($_COOKIE[$prev.'id']) && empty($_COOKIE[$prev.'pwd']) ? 'news/' : 'user/?action=userlobby'));
 	}
-
 ob_end_flush();
 ?>
