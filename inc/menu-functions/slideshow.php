04dc6132d95fb3c27d96cfce3f4bb95283dea260
Lucas <godkiller_nt@hammermaps.de>
2013-05-29 21:20:32 +0200

    Fix Slideshow

--- inc/menu-functions/slideshow.php
+++ inc/menu-functions/slideshow.php
@@ -3,18 +3,23 @@
 function slideshow()
 {
     global $db;
-    $pic = ''; $tabs = '';
     $qry = db("SELECT * FROM ".$db['slideshow']." ORDER BY `pos` ASC LIMIT 4");
-    while($get = _fetch($qry))
+    if(_rows($qry) >= 1)
     {
-        $target = ($get['target'] == "1" ? ",1" : "");
-        $pic .= show("menu/slideshowbild", array("image" => "<img src=\"../inc/images/slideshow/".$get['id'].".jpg\" alt=\"\" />",
-                                                 "link" => "'".$get['url']."'".$target,
-                                                 "bez" => $get['bez'],
-                                                 "text" => $get['desc']));
+        $pic = ''; $tabs = '';
+        while($get = _fetch($qry))
+        {
+            $target = ($get['target'] == "1" ? ",1" : "");
+            $pic .= show("menu/slideshowbild", array("image" => "<img src=\"../inc/images/slideshow/".$get['id'].".jpg\" alt=\"\" />",
+                                                     "link" => "'".$get['url']."'".$target,
+                                                     "bez" => $get['bez'],
+                                                     "text" => $get['desc']));
 
-        $tabs .= '<a href="#" class="slidertabs" id="slider'.$get['id'].'"></a>';
+            $tabs .= '<a href="#" class="slidertabs" id="slider'.$get['id'].'"></a>';
+        }
+
+        return show("menu/slideshow", array("pic" => $pic, "tabs" => $tabs));
     }
 
-    return show("menu/slideshow", array("pic" => $pic, "tabs" => $tabs));
+    return '';
 }
