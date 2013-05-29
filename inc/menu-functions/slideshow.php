4dea98dd3f820b4bc05b525bb9af21c28370ea3e
Lucas <godkiller_nt@hammermaps.de>
2013-05-29 22:40:33 +0200

    Slideshow presentation fix

--- inc/menu-functions/slideshow.php
+++ inc/menu-functions/slideshow.php
@@ -9,11 +9,17 @@ function slideshow()
         $pic = ''; $tabs = '';
         while($get = _fetch($qry))
         {
+            if(empty($get['desc']) && !$get['showbez'])
+                $slideroverlay = '';
+            else if(!empty($get['desc']) && !$get['showbez'])
+                $slideroverlay = '<div class="slideroverlay"><span>'.$get['desc'].'</span></div>';
+            else
+                $slideroverlay = '<div class="slideroverlay"><h2>'.$get['bez'].'</h2><span>'.$get['desc'].'</span></div>';
+
             $target = ($get['target'] == "1" ? ",1" : "");
             $pic .= show("menu/slideshowbild", array("image" => "<img src=\"../inc/images/slideshow/".$get['id'].".jpg\" alt=\"\" />",
                                                      "link" => "'".$get['url']."'".$target,
-                                                     "bez" => $get['showbez'] ? $get['bez'] : '',
-                                                     "text" => $get['desc']));
+                                                     "text" => $slideroverlay));
 
             $tabs .= '<a href="#" class="slidertabs" id="slider'.$get['id'].'"></a>';
         }
