3e830cf0c26b7055cf1a85ad597b8d50763bfe0e
Lucas <godkiller_nt@hammermaps.de>
2013-10-23 21:00:35 +0200

    Fix:  Issue #21

--- online/index.php
+++ online/index.php
@@ -26,7 +26,7 @@ $dir = "online";
                                              "whereami" => $whereami,
                                              "class" => $class));
   }
-		if(!empty($_GET['orderby']) && in_array($_GET['orderby'],array("whereami","autor"))) {
+		if(!empty($_GET['orderby']) && in_array($_GET['orderby'],array("whereami","ip"))) {
 	          $qry = db("SELECT * FROM ".$db['c_who']."
                          WHERE online+'".$useronline."'>'".time()."'
                          AND login = 0
@@ -37,6 +37,7 @@ $dir = "online";
                        AND login = 0
                        ORDER BY whereami");
   }
+  
   while($get = _fetch($qry))
   {
     if(!preg_match("#autor_#is",$get['whereami'])) $whereami = re($get['whereami']);
@@ -55,7 +56,7 @@ $dir = "online";
   $index = show($dir."/online", array("show" => $show,
                                       "head" => _online_head,
                                       "user" => _status_user.'/'._server_ip,
-									  "order_user" => orderby('autor'),
+									  "order_user" => orderby('ip'),
 									  "order_where" => orderby('whereami'),
                                       "where" => _online_whereami));
 
