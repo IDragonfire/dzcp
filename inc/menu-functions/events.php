6c8fb0cf338b3e4030bebba2c9fa4fa05c56d74c
acecom <acecom@freenet.de>
2014-03-12 21:11:29 +0100

    mouseover event hinzugefügt
    mouseover event hinzugefügt

--- inc/menu-functions/events.php
+++ inc/menu-functions/events.php
@@ -1,27 +1,28 @@
 <?php
-/**
- * DZCP - deV!L`z ClanPortal 1.6 Final
- * http://www.dzcp.de
- * Menu: Events
- */
-function events() {
-    global $db,$maxevent;
+function events()
+{
+  global $db,$maxevent;
 
-    $qry = db("SELECT `id`,`datum`,`title`,`event` FROM ".$db['events']."
-               WHERE `datum` > ".time()."
-               ORDER BY datum
-               LIMIT ".intval($maxevent)."");
+   $qry = db("SELECT id,datum,title,event FROM ".$db['events']."
+              WHERE datum > ".time()."
+              ORDER BY datum
+              LIMIT ".$maxevent."");
+   while($get = _fetch($qry))
+   {
+	
+     $events = show(_next_event_link, array("datum" => date("d.m.",$get['datum']),
+                                            "timestamp" => $get['datum'],
+                                            "event" => $get['title']));
 
-    $eventbox = '';
-    if(_rows($qry)) {
-        while($get = _fetch($qry)) {
-            $events = show(_next_event_link, array("datum" => date("d.m.",$get['datum']),
-                                                   "timestamp" => $get['datum'],
-                                                   "event" => $get['title']));
+   
+        $info = 'onmouseover="DZCP.showInfo(\''.jsconvert(re($get['title'])).'\', \''._kalender_uhrzeit.';'._datum.'\', \''.date("H:i", $get['datum'])._uhr.';'.date("d.m.Y", $get['datum']).'\')" onmouseout="DZCP.hideInfo()"';
+     
+  	    $eventbox .= show("menu/event", array("events" => $events,
+     								   "info" => $info));
+   
+   }
 
-            $eventbox .= show("menu/event", array("events" => $events));
-        }
-    }
 
-    return empty($eventbox) ? '<center style="margin:2px 0">'._no_events.'</center>' : '<table class="navContent" cellspacing="0">'.$eventbox.'</table>';;
+   return empty($eventbox) ? '<center style="margin:2px 0">'._no_events.'</center>' : '<table class="navContent" cellspacing="0">'.$eventbox.'</table>';;
 }
+?>
