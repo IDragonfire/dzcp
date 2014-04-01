a1c5888fe411bbd92e01fc1a984886f418b32fd5
Lucas <godkiller_nt@hammermaps.de>
2014-04-01 18:24:29 +0200

    Code finalizing: user/index.php * case lostpwd

--- user/index.php
+++ user/index.php
@@ -72,48 +72,37 @@ case 'login';
     }
 break;
 case 'lostpwd';
-  $where = _site_user_lostpwd;
-  if(!$chkMe)
-    {
+    $where = _site_user_lostpwd;
+    if(!$chkMe) {
         $index = show($dir."/lostpwd", array("head" => _lostpwd_head,
-                                                                                 "name" => _loginname,
-                                         "value" => _button_value_send,
-                                         "security" => _register_confirm,
-                                                                                 "email" => _email));
+                                             "name" => _loginname,
+                                             "value" => _button_value_send,
+                                             "security" => _register_confirm,
+                                             "email" => _email));
 
-        if($_GET['do'] == "sended")
-      {
-            $qry = db("SELECT id,user,level,pwd FROM ".$db['users']."
-                                 WHERE user= '".$_POST['user']."'
-                                 AND email = '".$_POST['email']."'");
-      $get = _fetch($qry);
+        if($do == "sended") {
+            $get = db_stmt("SELECT id,user,level,pwd FROM ".$db['users']." WHERE `user`= ? AND `email` = ?",
+                            array('ss', up($_POST['user']), up($_POST['email'])),false,true);
 
-            if(_rows($qry) && ($_POST['secure'] == $_SESSION['sec_lostpwd'] && $_SESSION['sec_lostpwd'] != NULL))
-          {
-        $pwd = mkpwd();
-        $upd = db("UPDATE ".$db['users']."
-                   SET `pwd` = '".md5($pwd)."'
-                   WHERE user = '".$_POST['user']."'
-                   AND email = '".$_POST['email']."'");
-
-        setIpcheck("pwd(".$get['id'].")");
-
-        $message = show(bbcode_email(settings('eml_pwd')), array("user" => $_POST['user'],"pwd" => $pwd));
-        $subject = re(settings('eml_pwd_subj'));
-        sendMail($_POST['email'],$subject,$message);
-
-              $index = info(_lostpwd_valid, "../user/?action=login");
+            if(_rows($qry) && ($_POST['secure'] == $_SESSION['sec_lostpwd'] && $_SESSION['sec_lostpwd'] != NULL)) {
+                $pwd = mkpwd();
+                db("UPDATE ".$db['users']." SET `pwd` = '".md5($pwd)."' WHERE id = '".$get['id']."'");
+                setIpcheck("pwd(".$get['id'].")");
+                $message = show(bbcode_email(settings('eml_pwd')), array("user" => $_POST['user'],"pwd" => $pwd));
+                $subject = re(settings('eml_pwd_subj'));
+                sendMail($_POST['email'],$subject,$message);
+                $index = info(_lostpwd_valid, "../user/?action=login");
             } else {
                 setIpcheck("trypwd(".$get['id'].")");
-
                 if($_POST['secure'] != $_SESSION['sec_lostpwd'] || empty($_SESSION['sec_lostpwd']))
-                  $index = error(_error_invalid_regcode,1);
-                else $index = error(_lostpwd_failed, 1);
+                    $index = error(_error_invalid_regcode,1);
+                else
+                    $index = error(_lostpwd_failed, 1);
             }
         }
-    } else {
-        $index = error(_error_user_already_in, 1);
     }
+    else
+        $index = error(_error_user_already_in, 1);
 break;
 case 'logout';
   $where = _site_user_logout;
@@ -3148,10 +3137,10 @@ break;
 endswitch;
 ## SETTINGS ##
 $whereami = preg_replace_callback("#autor_(.*?)$#",create_function('$id', 'return data("nick","$id[1]");'),$where);
-
 $title = $pagetitle." - ".$whereami."";
 $time_end = generatetime();
 $time = round($time_end - $time_start,4);
 page($index, $title, $where,$time);
+
 ## OUTPUT BUFFER END ##
 gz_output();
