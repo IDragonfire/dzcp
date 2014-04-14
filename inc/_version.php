8ad914ae68b5d1445571337da7e75729204a7fe3
Lucas <godkiller_nt@hammermaps.de>
2014-04-14 18:47:37 +0200

    Update Build Version

--- inc/_version.php
+++ inc/_version.php
@@ -1,7 +1,7 @@
 <?php
   define('_version', '1.6');
-  define('_release', '03.11.2013');
-  define('_build', '1600.01.04');
+  define('_release', '14.04.2014');
+  define('_build', '1600.02.00');
 
   $cp_color = '#d3d3d3';  ###########################################################################
                            # Hex Farbcode der Hintergrundfarbe Copyrightlinks am Ende der Homepage #
