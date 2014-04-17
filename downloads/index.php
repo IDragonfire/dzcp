6ca5c1306b5a559842939b6bced5321f97511a0c
Lucas <godkiller_nt@hammermaps.de>
2014-04-17 19:31:58 +0200

    Remove PageTimer & lang(); from Downloads

--- downloads/index.php
+++ downloads/index.php
@@ -8,8 +8,6 @@ include(basePath."/inc/config.php");
 include(basePath."/inc/bbcode.php");
 
 ## SETTINGS ##
-$time_start = generatetime();
-lang($language);
 $where = _site_dl;
 $title = $pagetitle." - ".$where."";
 $dir = "downloads";
@@ -196,9 +194,7 @@ switch ($action):
 endswitch;
 
 ## SETTINGS ##
-$time_end = generatetime();
-$time = round($time_end - $time_start,4);
-page($index, $title, $where,$time);
+page($index, $title, $where);
 
 ## OUTPUT BUFFER END ##
 gz_output();
