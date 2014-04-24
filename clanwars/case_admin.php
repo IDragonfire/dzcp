ba5f4a4ab35b74c2bfd597b1264b3a32d3c4887a
Lucas <godkiller_nt@hammermaps.de>
2014-04-24 22:21:47 +0200

    Outsourcing * Clanwars

---
+++ clanwars/case_admin.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * DZCP - deV!L`z ClanPortal 1.6 Final
+ * http://www.dzcp.de
+ */
+
+if(defined('_Clanwars')) {
+	if($do == 'edit') 
+		header("Location: ../admin/?admin=cw&do=edit&id=".$_GET['id']);
+}
