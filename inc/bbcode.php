5ce65c8e7b3f77e76a748bbc173a8d416e3740ca
Alper <lordalper@hotmail.de>
2014-04-29 01:19:51 +0200

    stylesheet fixes
    - remove margin-top from the mainContent class
    - Stylesheet fixes

--- inc/bbcode.php
+++ inc/bbcode.php
@@ -2345,7 +2345,7 @@ function page($index='',$title='',$where='',$wysiwyg='',$index_templ='index')
             $index = error(_error_have_to_be_logged, 1);
 
         $where = preg_replace_callback("#autor_(.*?)$#",create_function('$id', 'return data("nick","$id[1]");'),$where);
-        $index = empty($index) ? '' : (empty($check_msg) ? '' : $check_msg).'<table class="mainContent" cellspacing="1" style="margin-top:0">'.$index.'</table>';
+        $index = empty($index) ? '' : (empty($check_msg) ? '' : $check_msg).'<table class="mainContent" cellspacing="1">'.$index.'</table>';
 
         //-> Sort & filter placeholders
         //default placeholders
