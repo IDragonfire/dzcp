88872d917b5a233ba1703d512d80394e0aed8969
Lucas <godkiller_nt@hammermaps.de>
2014-08-07 19:17:50 +0200

    Add media plugin to tinymce plugin list

--- inc/_templates_/version1.6/_js/wysiwyg.uncompressed.js
+++ inc/_templates_/version1.6/_js/wysiwyg.uncompressed.js
@@ -1,14 +1,14 @@
-tinyMCE_GZ.init({ plugins : 'contextmenu,dzcp,advimage,paste,table,fullscreen,inlinepopups,spellchecker,searchreplace,insertdatetime,dzcp', themes : 'advanced', languages : (lng == 'de' ? lng : 'en'), disk_cache : true, debug : false });
+tinyMCE_GZ.init({ plugins : 'contextmenu,dzcp,advimage,paste,table,fullscreen,inlinepopups,spellchecker,searchreplace,insertdatetime,dzcp,media', themes : 'advanced', languages : (lng == 'de' ? lng : 'en'), disk_cache : true, debug : false });
 
 //default wysiwyg editor
 tinyMCE.init({
     theme                               : "advanced",
     mode                                : "specific_textareas",
     editor_selector                     : 'editorStyle',
-    plugins                             : 'contextmenu,dzcp,inlinepopups,spellchecker',
+    plugins                             : 'contextmenu,dzcp,inlinepopups,spellchecker,media',
     language                            : (lng == 'de' ? lng : 'en'),
     theme_advanced_buttons1: 'bold,italic,underline,|,justifyleft,justifycenter,justifyright,justifyfull,|,bullist,numlist,|,link,unlink,|,image,youtube,forecolor,'
-    + 'backcolor,|,smileys,flags,dzcpuser,spellchecker',
+    + 'backcolor,|,smileys,flags,dzcpuser,|,spellchecker',
     theme_advanced_toolbar_location     : 'top',
     theme_advanced_toolbar_align        : 'center',
     theme_advanced_statusbar_location   : 'bottom',
@@ -27,9 +27,9 @@ tinyMCE.init({
     mode                                : 'specific_textareas',
     editor_selector                     : 'editorStyleMini',
     theme                               : 'advanced',
-    plugins                             : 'contextmenu,dzcp,inlinepopups',
+    plugins                             : 'contextmenu,dzcp,inlinepopups,media',
     language                            : (lng == 'de' ? lng : 'en'),
-    theme_advanced_buttons1             : 'bold,italic,underline,|,link,unlink,|,image',
+    theme_advanced_buttons1             : 'bold,italic,underline,|,link,unlink',
     theme_advanced_buttons2             : '',
     theme_advanced_buttons3             : '',
     theme_advanced_toolbar_location     : 'top',
@@ -48,7 +48,7 @@ tinyMCE.init({
     mode                                : 'specific_textareas',
     editor_selector                     : 'editorStyleNewsletter',
     theme                               : 'advanced',
-    plugins                             : 'contextmenu,dzcp',
+    plugins                             : 'contextmenu,dzcp,media',
     language                            : (lng == 'de' ? lng : 'en'),
     theme_advanced_buttons1             : 'bold,italic,underline,|,justifyleft,justifycenter,justifyright,justifyfull,|,bullist,numlist,|,link,unlink,|,image,forecolor,backcolor',
     theme_advanced_buttons2             : '',
@@ -72,11 +72,11 @@ tinyMCE.init({
     editor_selector                   : 'editorStyleWord',
     theme                             : 'advanced',
     elements                          : "ajaxfilemanager",
-    plugins                           : 'contextmenu,dzcp,advimage,paste,table,fullscreen,inlinepopups,spellchecker,searchreplace,insertdatetime,',
+    plugins                           : 'contextmenu,dzcp,advimage,paste,table,fullscreen,inlinepopups,spellchecker,searchreplace,insertdatetime,media',
     language                          : (lng == 'de' ? lng : 'en'),
     theme_advanced_buttons1           : 'bold,italic,underline,|,justifyleft,justifycenter,justifyright,justifyfull,|,undo,redo,|,bullist,numlist,|,link,unlink,|,pastephp,|,forecolor,'
                                       + 'backcolor,|,smileys,flags,',
-    theme_advanced_buttons2           : 'paste,pastetext,pasteword,|,search,replace,|,image,|,tablecontrols,|,dzcpuser',
+    theme_advanced_buttons2           : 'paste,pastetext,pasteword,|,search,replace,|,image,|,tablecontrols,|,dzcpuser,|,media',
     theme_advanced_buttons3           : 'fontselect,fontsizeselect,|,insertdate,inserttime,|,sub,sup,|,outdent,indent,|,fullscreen,clip,spellchecker,code,youtube',
     extended_valid_elements           : 'img[class|src|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name|style],'
                                       + 'hr[class|width|size|noshade],font[face|size|color|style],span[class|align|style]',
