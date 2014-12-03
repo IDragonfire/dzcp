11c76a77db64839c8fe48e227f03b9fcc2e0c15b
Lucas <godkiller_nt@hammermaps.de>
2014-12-03 22:02:10 +0100

    Update Version

--- inc/_version.php
+++ inc/_version.php
@@ -5,6 +5,6 @@
  */
 
 define('_version', '1.6.1.0');
-define('_release', '23.11.2014');
-define('_build', '1610.02.26');
+define('_release', '03.12.2014');
+define('_build', '1610.02.36');
 define('_edition', 'dev');
